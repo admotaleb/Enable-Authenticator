88d3ad79ecc26faca86c5edc654c03a678c57e86
Motaleb <Motaleb@DESKTOP-9T8RV0S>
2022-08-29 23:00:51 +0600

    Add project files.

---
+++ OtpIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OtpIdentity.Data;
+
+[assembly: HostingStartup(typeof(OtpIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace OtpIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ OtpIdentity/Data/Migrations/20220827053053_Inishial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtpIdentity.Data.Migrations
+{
+    public partial class Inishial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
